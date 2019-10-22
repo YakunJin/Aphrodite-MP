838c6629c27ac29de1e4e220d470bafadd84c3c4
ykjin <jinyakun0629@gmail.com>
2019-10-22 23:14:24 +0800

    login popup

---
+++ miniprogram/components/login/login.js
@@ -0,0 +1,52 @@
+// components/login/login.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    onGetUserInfo: function (e) {
+      if (!this.data.logged && e.detail.userInfo) {
+        this.setData({
+          logged: true,
+          avatarUrl: e.detail.userInfo.avatarUrl,
+          userInfo: e.detail.userInfo
+        })
+      }
+    },
+
+    onGetOpenid: function () {
+      // 调用云函数
+      wx.cloud.callFunction({
+        name: 'login',
+        data: {},
+        success: res => {
+          console.log('[云函数] [login] user openid: ', res.result.openid)
+          app.globalData.openid = res.result.openid
+          wx.navigateTo({
+            url: '../userConsole/userConsole',
+          })
+        },
+        fail: err => {
+          console.error('[云函数] [login] 调用失败', err)
+          wx.navigateTo({
+            url: '../deployFunctions/deployFunctions',
+          })
+        }
+      })
+    },
+  }
+})
